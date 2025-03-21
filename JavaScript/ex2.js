b28dd260907325e966883b17f0a9ced42827b262
arnob-webber <billahamid26@gmail.com>
2025-03-21 20:03:03 +0600

    ex2

---
+++ JavaScript/ex2.js
@@ -0,0 +1,9 @@
+// Create two variables, a and b, assign them numbers, and print their sum.
+
+let a, b; /**Create two variables */
+
+a = 5;
+b = 5;
+
+const sum = a + b; /**and print their sum. */
+console.log(sum);
