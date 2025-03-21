d3e2a78bb5443413a71803abab018d9e4046adbc
arnob-webber <billahamid26@gmail.com>
2025-03-21 20:14:20 +0600

    js ex- arnob

---
+++ JavaScript/ex3.js
@@ -0,0 +1,8 @@
+// Write a program to find the length of a string stored in a variable str.
+
+let ohGirl = "all girls are crazy";
+// count all spaces
+
+let checkChracter = ohGirl.length;
+
+console.log(checkChracter);
