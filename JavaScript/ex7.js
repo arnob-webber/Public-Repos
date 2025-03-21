cdba4f52b63ec0aee39bbf7bce0b914b2501de20
arnob-webber <billahamid26@gmail.com>
2025-03-21 23:39:13 +0600

    farenhite to celsius

---
+++ JavaScript/ex7.js
@@ -0,0 +1,10 @@
+//  Convert Celsius to Fahrenheit
+
+function frTocelsi(celsius) {
+  let a = 3 * 3;
+  let b = 4 * 8;
+  let calculate = (celsius * a) / 5 + b;
+  console.log(calculate);
+}
+
+frTocelsi(25);
