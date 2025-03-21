b54bd6cdefe3a37e43e138b98a6bbe2d2b4439eb
arnob-webber <billahamid26@gmail.com>
2025-03-21 23:23:36 +0600

    parametter

---
+++ JavaScript/ex6.js
@@ -0,0 +1,12 @@
+// Create a function greet that accepts a name as a parameter and returns "Hello, [name]!"
+let x = 22;
+// let ohhBoy;
+if (x > 18) {
+  ohhBoy = "arnob";
+} else {
+  console.log("get lost");
+}
+greet(ohhBoy);
+function greet(name) {
+  console.log("welcome" + name);
+}
