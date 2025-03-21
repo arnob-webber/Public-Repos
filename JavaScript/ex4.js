3105d61b0a83183b524374c68ecbccdec666aedd
arnob-webber <billahamid26@gmail.com>
2025-03-21 23:03:58 +0600

    odd and even with loop for

--- JavaScript/ex4.js
+++ JavaScript/ex4.js
@@ -1,12 +1,21 @@
 // Create a variable num and check if it is even or odd.
-
+/**let sum = X % 2; modules operetor % */
 let X = 9;
 let x = 8;
-
-/**let sum = X % 2; modules operetor % */
 
 if (X % 2 === 0) {
   console.log("odd");
 } else {
   console.log("odd");
+}
+
+// with for loop
+
+let z = 1;
+for (; z <= 10; z++) {
+  if (z % 2 === 0) {
+    console.log(z + "= even");
+  } else {
+    console.log(z + "= Odd");
+  }
 }
