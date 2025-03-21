1ecaee42aae42d355baca770bbcd30ed17b344da
arnob-webber <billahamid26@gmail.com>
2025-03-21 23:09:51 +0600

    area of a rectagnle

---
+++ JavaScript/ex5.js
@@ -0,0 +1,10 @@
+//  Write a program to calculate the area of a rectangle given length and width
+//  Area=Length×Width
+
+const roomWidth = 60;
+const roomLength = 20;
+
+//  Area=Length×Width. its middle class math formula
+const totalArea = roomWidth * roomLength;
+
+console.log(totalArea);
