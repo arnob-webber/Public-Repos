6032d4282a4777692a6ad8edebbebc54c467d4c0
arnob-webber <billahamid26@gmail.com>
2025-03-21 22:58:18 +0600

    ood even

---
+++ JavaScript/ex4.js
@@ -0,0 +1,12 @@
+// Create a variable num and check if it is even or odd.
+
+let X = 9;
+let x = 8;
+
+/**let sum = X % 2; modules operetor % */
+
+if (X % 2 === 0) {
+  console.log("odd");
+} else {
+  console.log("odd");
+}
