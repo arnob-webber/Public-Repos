3d25b0fe2710fd4916e44863e0f05c10051e4a2f
arnob-webber <billahamid26@gmail.com>
2025-03-22 23:46:49 +0600

    code

---
+++ ex12.js
@@ -0,0 +1,11 @@
+// Write a program to sum all elements in an array.
+
+let myArray = [1, 2, 3, 4, 5, 6];
+let y = 0;
+myArray.forEach((x) => (y += x));
+console.log(y);
+
+// let myArray = [1, 2, 3, 4, 5, 6];
+// let y = 0;
+// myArray.forEach((x) => (y += x));
+// console.log(y);
---
+++ ex13.js
@@ -0,0 +1,10 @@
+// Create an object student with properties name, age, and grade.
+// Access and print each property.
+
+let student = {
+  name: "arnob",
+  age: 69,
+  roll: 96,
+};
+
+console.log(student);
